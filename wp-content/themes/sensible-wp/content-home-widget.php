eeeb4ec422dbbbf8cfd80be66b6e9b1f7f6e7f31
melmiranda <melania.smc@hotmail.com>
2017-02-13 11:11:52 -0300

    11:11 13-02-2017

--- wp-content/themes/sensible-wp/content-home-widget.php
+++ wp-content/themes/sensible-wp/content-home-widget.php
@@ -7,50 +7,50 @@
 ?>
 
 
-		<div class="home-cta" style="background: url(<?php echo esc_url( get_theme_mod( 'sensiblewp_new_widget_area_background' )); ?>) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">  
+		<div class="home-cta" style="background: url(<?php echo esc_url( get_theme_mod( 'sensiblewp_new_widget_area_background' )); ?>) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
         	<div class="grid grid-pad">
             	<div class="sensible-home-widget-area">
-                    
+
                     	<div class="col-1-1">
-                
+
                 			<?php if ( get_theme_mod( 'home_widget_icon' ) ) : ?>
-                            
+
                 				<i class="fa <?php echo esc_html( get_theme_mod( 'home_widget_icon' )); ?>"></i>
-                                
-							<?php endif; ?> 
-                    
-                    	</div><!-- col-1-1 --> 
-                    
-                    	<div class="col-1-1">
-                    
-                    		<?php if ( is_active_sidebar('home-widget') ) : ?>
-                    
-    							<?php dynamic_sidebar('home-widget'); ?> 
-                           
-    		 				<?php endif; ?>
-                    
+
+							<?php endif; ?>
+
                     	</div><!-- col-1-1 -->
-    
+
                     	<div class="col-1-1">
-                    
+
+                    			<p>NUESTRA HISTORIA</p>
+
+														<p>Su estadio de básquet, el Osvaldo Casanova tiene capacidad para 3950...</p>
+
+														<button class="outline white">Ver más</button>
+
+                    	</div><!-- col-1-1 -->
+
+                    	<div class="col-1-1">
+
                     		<?php if ( get_theme_mod( 'sensiblewp_widget_button_url' ) ) : ?>
-                    			
-                                <a href="<?php echo esc_url( get_page_link( get_theme_mod('sensiblewp_widget_button_url')))?>"> 
-                        			
-									<?php if ( get_theme_mod( 'sensiblewp_widget_button_text' ) ) : ?> 
-                            			
+
+                                <a href="<?php echo esc_url( get_page_link( get_theme_mod('sensiblewp_widget_button_url')))?>">
+
+									<?php if ( get_theme_mod( 'sensiblewp_widget_button_text' ) ) : ?>
+
                                         <button class="outline white">
 											<?php echo esc_html( get_theme_mod( 'sensiblewp_widget_button_text' )); ?>
                                         </button>
-									
-									<?php endif; ?> 
-                                    
+
+									<?php endif; ?>
+
                         		</a>
-                                
-                    		<?php endif; ?> 
-                    
-                    	</div><!-- col-1-1 --> 
-                    
-            	</div><!-- sensible-home-widget-area --> 
+
+                    		<?php endif; ?>
+
+                    	</div><!-- col-1-1 -->
+
+            	</div><!-- sensible-home-widget-area -->
 			</div><!-- grid -->
-        </div><!-- home-cta --> 
+        </div><!-- home-cta -->
--- wp-content/themes/sensible-wp/content-intro.php
+++ wp-content/themes/sensible-wp/content-intro.php
@@ -7,22 +7,22 @@
 ?>
 
 
-			<div class="home-content">
+			<div class="home-content" style="background-color:#e8f1fc">
         		<div class="grid grid-pad">
         			<div class="col-1-1">
-                		
+
 						<?php if ( get_theme_mod( 'intro_text' ) ) : ?>
-        			  		
+
                             <h6 class="wow animated fadeInLeft"><?php echo wp_kses_post( get_theme_mod( 'intro_text' )); ?></h6>
-                		
-						<?php endif; ?>
-                        
-                        <?php if ( get_theme_mod( 'intro_textbox' ) ) : ?>
-        			  		
-                            <p class="wow animated fadeInRight"><?php echo wp_kses_post( get_theme_mod( 'intro_textbox' )); ?></p>  
-                		
+
 						<?php endif; ?>
 
-                	</div><!-- col-1-1 --> 
+                        <?php if ( get_theme_mod( 'intro_textbox' ) ) : ?>
+
+                            <p class="wow animated fadeInRight"><?php echo wp_kses_post( get_theme_mod( 'intro_textbox' )); ?></p>
+
+						<?php endif; ?>
+
+                	</div><!-- col-1-1 -->
         		</div><!-- grid -->
         	</div><!-- home-content -->

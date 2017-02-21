d00ac1e3b5b1cf46a6204ccc38db89b3953eee5a
melmiranda <melania.smc@hotmail.com>
2017-02-21 12:12:39 -0300

    12:12 21-02-2017

--- wp-content/themes/sensible-wp/page-contact.php
+++ wp-content/themes/sensible-wp/page-contact.php
@@ -163,6 +163,9 @@ img.emoji {
 </ul></div>
 	</nav>
 
+
+
+
 	<section id="content" class="site-content">
 
 
@@ -177,9 +180,9 @@ img.emoji {
 
 
 
-	<div class="grid grid-pad">
+	<div class="">
 
-		<div class="col-9-12">
+		<div class="">
 				<iframe style="width:100%; height:250px;"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10472.34584621621!2d-62.271434748342266!3d-38.707544080170756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95edbcabdc1302bd%3A0x76d1d88d241e7a11!2sBah%C3%ADa+Blanca%2C+Buenos+Aires!5e0!3m2!1ses!2sar!4v1487254124596" frameborder="0" style="border:0" allowfullscreen></iframe>
 		</div>
 
